a5bb71a553f82b7ce58bcc33f7f729297a2992e4
Starrodkirby86 <majestic.mudkip@gmail.com>
2025-05-23 11:26:20 -0700

    Remove use of explicit any

--- src/app/components/TipsAndTricks/content/mdx-components.tsx
+++ src/app/components/TipsAndTricks/content/mdx-components.tsx
@@ -1,23 +1,24 @@
 'use client';
 
 import { MDXProvider } from '@mdx-js/react';
+import { ComponentPropsWithoutRef } from 'react';
 
 const components = {
-  h1: (props: any) => (
+  h1: (props: ComponentPropsWithoutRef<'h1'>) => (
     <h1 {...props} className='text-4xl font-display text-theme-02 mb-6' />
   ),
-  h2: (props: any) => (
+  h2: (props: ComponentPropsWithoutRef<'h2'>) => (
     <h2 {...props} className='text-2xl font-body font-extrabold uppercase text-theme-01 mb-4' />
   ),
-  p: (props: any) => <p {...props} className='text-lg text-theme-01 mb-4' />,
-  ul: (props: any) => (
+  p: (props: ComponentPropsWithoutRef<'p'>) => <p {...props} className='text-lg text-theme-01 mb-4' />,
+  ul: (props: ComponentPropsWithoutRef<'ul'>) => (
     <ul
       {...props}
       className='list-disc list-inside space-y-2 text-theme-01 mb-4'
     />
   ),
-  li: (props: any) => <li {...props} className='text-lg' />,
-  strong: (props: any) => <b {...props} className='font-bold text-theme-05' />,
+  li: (props: ComponentPropsWithoutRef<'li'>) => <li {...props} className='text-lg' />,
+  strong: (props: ComponentPropsWithoutRef<'strong'>) => <b {...props} className='font-bold text-theme-05' />,
 };
 
 export function MDXComponents({ children }: { children: React.ReactNode }) {
